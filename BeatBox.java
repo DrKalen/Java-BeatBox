c0b20e17c5169a8f8b5c1164e8a7c0b1b7779ae5
Kalen Hammann <kalen7@gmail.com>
2019-03-25 16:23:32 -0400

    get the percussion sounds playing

--- BeatBox.java
+++ BeatBox.java
@@ -9,6 +9,7 @@ public class BeatBox {
   JPanel mainPanel;
   ArrayList<JCheckBox> checkboxList;
   Sequencer sequencer;
+  Sequence sequence;
   Track track;
   JFrame theFrame;
 
@@ -68,32 +69,102 @@ public class BeatBox {
 	  mainPanel.add(c);
 	}
 
+	setUpMidi();
+
 	theFrame.setBounds(50,50,300,300);
 	theFrame.pack();
 	theFrame.setVisible(true);
-  }
+  } // close method
+
+  public void setUpMidi() {
+    try {
+    	sequencer = MidiSystem.getSequencer();
+	sequencer.open();
+	sequence = new Sequence(Sequence.PPQ,4);
+	track = sequence.createTrack();
+	sequencer.setTempoInBPM(120);
+
+    } catch (Exception e) {e.printStackTrace();}
+  } // close method
+
+  public void buildTrackAndStart() {
+	int[] trackList = null;
+
+	sequence.deleteTrack(track);
+	track = sequence.createTrack();
+
+	for(int i = 0; i < 16; i++) {
+	  trackList = new int[16];
+	  int key = instruments[i];	
+	
+	  for (int j = 0; j < 16; j++) {
+		JCheckBox jc = checkboxList.get(j + 16*i);
+		if(jc.isSelected()) {
+		  trackList[j] = key;
+		} else {
+		  trackList[j] = 0;
+		}
+	  } //close inner loop
+
+	  makeTracks(trackList);
+	  track.add(makeEvent(176,1,127,0,16));
+	} //close outer
+	
+	track.add(makeEvent(192,9,1,0,15));
+	try {
+
+	  sequencer.setSequence(sequence);
+	  sequencer.setLoopCount(sequencer.LOOP_CONTINUOUSLY);
+	  sequencer.start();
+	  sequencer.setTempoInBPM(120);
+
+	} catch(Exception e) {e.printStackTrace(); }
+  } // close buildTrackAndStart method
 
   public class MyStartListener implements ActionListener {
 	public void actionPerformed(ActionEvent a) {
-	
+	  buildTrackAndStart();
 	}
-  }
+  } // close inner class
 
   public class MyStopListener implements ActionListener {
 	public void actionPerformed(ActionEvent a) {
-
+	  sequencer.stop();
 	}
-  }
+  } // close inner class
 
   public class MyUpTempoListener implements ActionListener {
 	public void actionPerformed(ActionEvent a) {
-
+	  float tempoFactor = sequencer.getTempoFactor();
+	  sequencer.setTempoFactor((float)(tempoFactor * 1.03));
 	}
-  }
+  } // close inner class
 
   public class MyDownTempoListener implements ActionListener {
 	public void actionPerformed(ActionEvent a) {
-
+	  float tempoFactor = sequencer.getTempoFactor();
+	  sequencer.setTempoFactor((float)(tempoFactor * .97));
 	}	
+  } // close inner class
+
+  public void makeTracks(int[] list) {
+	for (int i = 0; i < 16; i++) {
+	  int key = list[i];
+	
+	  if (key != 0) {
+	  	track.add(makeEvent(144,9,key, 100, i));
+		track.add(makeEvent(128,9,key, 100, i+1));
+	  }
+	}
   }
-}
+
+  public MidiEvent makeEvent(int comd, int chan, int one, int two, int tick) {
+	MidiEvent event = null;
+	try {
+	  ShortMessage a = new ShortMessage();
+	  a.setMessage(comd, chan, one, two);
+	  event = new MidiEvent(a, tick);
+	} catch (Exception e) {e.printStackTrace();}
+	  return event;
+  }
+} // close class
